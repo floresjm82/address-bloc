016c7d529eadfa0f39db05d25280eafd6a601d28
Jorge <floresjmjr@gmail.com>
2015-08-16 23:22:55 -0400

    Created greeting method

--- greeting.rb
+++ greeting.rb
@@ -1,9 +1,10 @@
 def greeting
-	ARGV.each_with_index do |name, index|
-		if index != 0
-			puts "#{ARGV.first} #{name}"
-		end
-	end
+  ARGV.each_with_index do |name, index|
+  greet = ARGV[0]
+    if index != 0
+      puts "#{greet} #{name}"
+    end
+  end
 end
 
 greeting
