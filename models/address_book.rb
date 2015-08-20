584f764c0065c8aa9b425f0d0404cdc6b34f9de6
Jorge <floresjmjr@gmail.com>
2015-08-19 23:47:04 -0400

    add a remove entry method

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,13 +17,18 @@ class AddressBook
 #10 We compare name with the name of the current entry.  If name
 #lexicographically proceeds entry.name, we've found the index to insert at.
 #Otherwise we increment index and continue comparing with the other entries.
-      if name < entry.name
-        break
-      end
+        if name < entry.name
+          break
+        end
       index += 1
-    end
+      end
 #11 We insert a new entry into entries using the caluclated index.
-    @entries.insert(index, Entry.new(name, phone_number, email))
+      @entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    @entries.delete_if {|entry| entry.name == name}
+  end
+
 
 end
